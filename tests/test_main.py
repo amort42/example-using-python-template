cad61ec6f255aaf447c5604ce225356c3bc0ab36
Shane O'Connell <shaneoconnell@outlook.com>
2024-01-31 20:23:54 +0000

    run pre-commit

--- tests/test_main.py
+++ tests/test_main.py
@@ -6,12 +6,12 @@ def test_main(capsys):
     """
     Test the main function with a valid name.
 
-    Args: 
+    Args:
         capsys: pytest fixture to capture stdout and stderr.
     """
     assert main(["--name", "test"]) == 0
     out, err = capsys.readouterr()
-    
+
     # test is a user which does not have any contributions since 2010.
     # Hopefully this will not change in the future and the test will not break.
     assert out == "Hello test!\nYou have 5 repos.\n"
